f697ee4bd188361cae5d368a1027de948a99dfda
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2023-08-31 22:32:03 -0300

    Fundamentos de DDD - Mapeando propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,14 @@
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface AnswerProps {
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
   content: string
-  authorId: string
-  questionId: string
+  upvotes: number
+  downvotes: number
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
